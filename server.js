da75596604c79bac94887b9166232c02e78c1a73
ages25 <ernest.40frimpong@gmail.com>
2024-01-26 15:54:32 +0000

    done

--- server.js
+++ server.js
@@ -1,47 +1,47 @@
 const express = require('express');
 const app = express();
-const port = 3500;
+const port = process.env.PORT || 3500;
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', (req, res) =>{
+app.get('/', (req, res) => {
     res.render('index');
 });
 
-app.get('/about', (req, res) =>{
+app.get('/about', (req, res) => {
     res.render('about');
 });
 
-app.get('/contact', (req, res) =>{
+app.get('/contact', (req, res) => {
     res.render('contact');
 });
 
-app.get('/blog', (req, res) =>{
+app.get('/blog', (req, res) => {
     res.render('blog');
 });
 
-app.get('/elements', (req, res) =>{
+app.get('/elements', (req, res) => {
     res.render('elements');
 });
 
-app.get('/job_details', (req, res) =>{
+app.get('/job_details', (req, res) => {
     res.render('job_details');
 });
 
-app.get('/job_listing', (req, res) =>{
+app.get('/job_listing', (req, res) => {
     res.render('job_listing');
 });
 
-app.get('/single-blog', (req, res) =>{
+app.get('/single-blog', (req, res) => {
     res.render('single-blog');
 });
 
 // Handle 404 error - Page not found
-app.use((req, res) =>{
+app.use((req, res) => {
     res.status(404).send('Page not found');
 });
 
-app.listen(port,()=>{
-    console.log(`Program running on http://localhost:${port}`);
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
 });
